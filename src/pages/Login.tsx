f22a56bf9ecd884227887267e0e6d5bd18e096d3
GGTuran <tayyabturan67@gmail.com>
2024-07-05 18:42:42 +0600

    added login redirect,logout and toast

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,44 +1,55 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
   const { register, handleSubmit } = useForm({
-    defaultValues:{
-        userId: 'A-0001',
-        password: 'admin123'
-    }
+    defaultValues: {
+      userId: "A-0001",
+      password: "admin123",
+    },
   });
+
+  const navigate = useNavigate();
 
   const dispatch = useAppDispatch();
 
-  const [login, { error}] = useLoginMutation();
-   
-  console.log('error => ',error);
+  const [login] = useLoginMutation();
 
-  const onSubmit = async (data) => {
-    const userInfo = {
+  const onSubmit = async (data: FieldValues) => {
+    const toastId = toast.loading("Logging in!");
+
+    try {
+      const userInfo = {
         id: data.userId,
-        password: data.password
-    };
-    const res = await login(userInfo).unwrap();
-    const user = verifyToken(res.data.accessToken);
-    console.log(user)
-    dispatch(setUser({ user: user, token: res.data.accessToken }))
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
+      console.log(user);
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success("Logged in", { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!", { id: toastId, duration: 2000 });
+    }
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="id">ID: </label>
-        <input type="text" id="id"  {...register('userId')}/>
+        <input type="text" id="id" {...register("userId")} />
       </div>
       <div>
         <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register('password')} />
+        <input type="text" id="password" {...register("password")} />
       </div>
       <Button htmlType="submit">Login</Button>
     </form>
--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -2,8 +2,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../store";
 
+export type TUser = {
+    userId: string;
+    role: string;
+    iat: number;
+    exp: number;
+}
+
 type TAuthState = {
-    user: null | object;
+    user: null | TUser;
     token: null | string;
 };
 
@@ -28,7 +35,7 @@ const authSlice = createSlice({
     },
 });
 
-export const { setUser, logOut} = authSlice.actions
+export const { setUser, logOut } = authSlice.actions
 
 export default authSlice.reducer;
 
