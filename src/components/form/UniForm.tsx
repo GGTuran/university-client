c936d4cc198b866765b48bc3c64d83570ead71ff
GGTuran <tayyabturan67@gmail.com>
2024-07-06 12:54:29 +0600

    added form wrapper

--- src/components/form/UniForm.tsx
+++ src/components/form/UniForm.tsx
@@ -1,13 +1,35 @@
-import { useForm } from "react-hook-form";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ReactNode } from "react";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
 
-const UniForm = ({ onSubmit, children }) => {
+type TFormConfig = {
+    defaultValues?: Record<string,any>; 
+};
 
-    const { handleSubmit } = useForm();
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {children}
-        </form>
-    );
+type TFormProps = {
+  onSubmit: SubmitHandler<FieldValues>;
+  children: ReactNode;
+} & TFormConfig;
+
+const UniForm = ({ onSubmit, children, defaultValues }: TFormProps) => {
+    const formConfig: TFormConfig = {};
+
+    if(defaultValues){
+        formConfig['defaultValues'] = defaultValues;
+    }
+
+  const methods = useForm(formConfig);
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
 };
 
 export default UniForm;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Button } from "antd";
-import { FieldValues, useForm } from "react-hook-form";
+import { Button, Row } from "antd";
+import { FieldValues } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
 import { setUser, TUser } from "../redux/features/auth/authSlice";
@@ -8,14 +8,20 @@ import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import UniForm from "../components/form/UniForm";
+import UniInput from "../components/form/UniInput";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      userId: "A-0001",
-      password: "admin123",
-    },
-  });
+  // const { register, handleSubmit } = useForm({
+  //   defaultValues: {
+  //     userId: "A-0001",
+  //     password: "admin123",
+  //   },
+  // });
+
+  const defaultValues = {
+    userId: "A-0001",
+    password: "admin123",
+  };
 
   const navigate = useNavigate();
 
@@ -24,38 +30,34 @@ const Login = () => {
   const [login] = useLoginMutation();
 
   const onSubmit = async (data: FieldValues) => {
-    console.log(data)
-    // const toastId = toast.loading("Logging in!");
+    console.log(data);
+    const toastId = toast.loading("Logging in!");
 
-    // try {
-    //   const userInfo = {
-    //     id: data.userId,
-    //     password: data.password,
-    //   };
-    //   const res = await login(userInfo).unwrap();
-    //   const user = verifyToken(res.data.accessToken) as TUser;
-    //   console.log(user);
-    //   dispatch(setUser({ user: user, token: res.data.accessToken }));
-    //   toast.success("Logged in", { id: toastId, duration: 2000 });
-    //   navigate(`/${user.role}/dashboard`);
-    // } catch (error) {
-    //   console.log(error);
-    //   toast.error("Something went wrong!", { id: toastId, duration: 2000 });
-    // }
+    try {
+      const userInfo = {
+        id: data.userId,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
+      console.log(user);
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success("Logged in", { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!", { id: toastId, duration: 2000 });
+    }
   };
 
   return (
-    <UniForm onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register("userId")} />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register("password")} />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </UniForm>
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <UniForm onSubmit={onSubmit} defaultValues={defaultValues}>
+        <UniInput type="text" name="userId" label="ID: "></UniInput>
+        <UniInput type="text" name="password" label="Password: "></UniInput>
+        <Button htmlType="submit">Login</Button>
+      </UniForm>
+    </Row>
   );
 };
 
