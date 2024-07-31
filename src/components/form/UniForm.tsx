fafe665f700efc561d41332792589609d64daf30
GGTuran <tayyabturan67@gmail.com>
2024-07-31 18:23:14 +0600

    validations added

--- src/components/form/UniForm.tsx
+++ src/components/form/UniForm.tsx
@@ -10,6 +10,7 @@ import {
 
 type TFormConfig = {
     defaultValues?: Record<string,any>; 
+    resolver?: any;
 };
 
 type TFormProps = {
@@ -17,11 +18,15 @@ type TFormProps = {
   children: ReactNode;
 } & TFormConfig;
 
-const UniForm = ({ onSubmit, children, defaultValues }: TFormProps) => {
+const UniForm = ({ onSubmit, children, defaultValues, resolver, }: TFormProps) => {
     const formConfig: TFormConfig = {};
 
     if(defaultValues){
         formConfig['defaultValues'] = defaultValues;
+    }
+
+    if(resolver){
+      formConfig['resolver'] = resolver;
     }
 
   const methods = useForm(formConfig);
--- src/components/form/UniSelect.tsx
+++ src/components/form/UniSelect.tsx
@@ -2,28 +2,28 @@ import { Form, Select } from "antd";
 import { Controller } from "react-hook-form";
 
 type TUNISelectProps = {
-    label: string;
-    name: string;
-    options: { value: string; label: string; disabled?: boolean }[];
-  };
+  label: string;
+  name: string;
+  options: { value: string; label: string; disabled?: boolean }[];
+};
 
 const UniSelect = ({ label, name, options }: TUNISelectProps) => {
-    return (
-        <Controller
-        name={name}
-        render={({ field, fieldState: { error } }) => (
-          <Form.Item label={label}>
-            <Select
-              style={{ width: '100%' }}
-              {...field}
-              options={options}
-              size="large"
-            />
-            {error && <small style={{ color: 'red' }}>{error.message}</small>}
-          </Form.Item>
-        )}
-      />
-    );
+  return (
+    <Controller
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <Form.Item label={label}>
+          <Select
+            style={{ width: "100%" }}
+            {...field}
+            options={options}
+            size="large"
+          />
+          {error && <small style={{ color: "red" }}>{error.message}</small>}
+        </Form.Item>
+      )}
+    />
+  );
 };
 
 export default UniSelect;
